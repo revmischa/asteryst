2a73c2bc956f72151cce4b7016e63701396350d7
Mischa S <revmischa@cpan.org>
2012-06-04 18:11:14 -0700

    irc msg, config

--- lib/Asteryst/AGI/Controller/AMT.pm
+++ lib/Asteryst/AGI/Controller/AMT.pm
@@ -16,8 +16,18 @@ sub LOAD_CONTROLLER {
 sub ring_space {
     my ($self, $c) = @_;
 
+    my $cid_num = $c->session->caller_id_num || '';
+    my $cid_name = $c->session->caller_id_name || '';
+    my $dnid = $c->session->dnid || '<Unknown>';
+
+    # obfuscate cid_num
+    $cid_num = substr($cid_num, 1, 3) . '-XXX-XXXX'; # just area code
+    my $cid = $cid_num;
+    $cid .= " <$cid_num>" if $cid_num;
+
     $c->log(3, "Someone is calling the space!");
-    $c->forward('/AMT/irc_notify');
+    my $msg = "Incoming call from \033[33m$cid\033[0m to extension \033[1;14m$dnid\033[0m";
+    $c->forward('/AMT/irc_notify', msg => $msg);
     $c->agi->exec('Dial', 'SIP/wooster&SIP/obitalk');
 }
 
@@ -27,13 +37,18 @@ sub irc_notify {
 
     fork and return;
 
+    my $config  = $c->config;
+    my $server  = $config->{irc}{server} or die "irc.server not defined in config";
+    my $nick    = $config->{irc}{nick} or die "irc.nick not defined in config";
+    my $channel = $config->{irc}{channel} or die "irc.channel not defined in config";
+
     my $cv = AnyEvent->condvar;
     my $con = new AnyEvent::IRC::Client;
 
     $con->reg_cb(disconnect => sub { $cv->broadcast });
     $con->send_srv(
-        PRIVMSG => '#ttt',
-        "Hello there I'm the cool AnyEvent::IRC test script!"
+        PRIVMSG => $channel,
+        $args{msg},
     );
     $con->reg_cb (
         sent => sub {
@@ -42,7 +57,7 @@ sub irc_notify {
             }
         }
     );
-    $con->connect("irc.hardchats.com", 6667, { nick => 'ToyFone' });
+    $con->connect($server, 6667, { nick => $nick });
     $cv->wait;
     $con->disconnect;
 }
